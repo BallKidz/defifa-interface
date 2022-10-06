8f7428c251a0887cb7624128624697d754a59b29
blazh <medenicablazo@gmail.com>
2022-10-07 01:02:32 +0200

    testing original sizes from figma

--- src/components/Navbar/Info/Treasury/index.tsx
+++ src/components/Navbar/Info/Treasury/index.tsx
@@ -1,9 +1,15 @@
 import { colors } from "../../../../constants/colors";
+import styles from "./Treasury.module.css";
 
 const Treasury = () => {
   return (
-    <div>
-      <h1 style={{ color: colors.turquoise }}>0 eth from 0 players</h1>
+    <div className={styles.container}>
+      <h1 style={{ color: colors.turquoise }}>
+        0 eth
+        <span> from</span>
+        <span> 0 players</span>
+      </h1>
+
       <p style={{ color: colors.purple }}>current game treasury</p>
     </div>
   );
