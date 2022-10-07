09548f741524cfff44fc9b425b3cac3dbcc9c391
Devian <devianyeth@gmail.com>
2022-10-07 13:21:11 +0530

    minor changes

--- src/components/Rules/index.tsx
+++ src/components/Rules/index.tsx
@@ -14,19 +14,19 @@ const Rules = () => {
             FIFA WORLD CUP.
             <br />
             <br />
-            MINT TEAM NFTS TO INCREASE THE GAME’S TREASURY, HERE.
+            MINT TEAM NFTS TO INCREASE THE GAME’S TREASURY.
             <br />
             <br />
             THE NFT’S ARE A CLAIM ON THIS TREASURY. <br />
             <br />
-            YOU CAN GET A FULL REFUND ANYTIME BEFORE THE GAME STARTS, HERE.
+            YOU CAN GET A FULL REFUND ANYTIME BEFORE THE GAME STARTS.
           </p>
         </div>
         <div className={styles.phaseBox}>
           <h1>PHASE 2: START</h1>
           <p>
             THE TREASURY IS LOCKED AND MINTING PERMENENTLY ENDS BEFORE THE FIRST
-            KICKOFF ON NOVERMBER 20, 2022.
+            KICKOFF ON NOVEMBER 20, 2022.
             <br />
             <br />
             HOLDERS OF EACH TEAM’S NFTS WORK TOGETHER TO GUESS THE OUTCOME OF
@@ -51,20 +51,23 @@ const Rules = () => {
           <h1>PHASE 4: END</h1>
           <p>
             THE GAME IS SELF REFEREE’D. <br />
-            <br />A FINAL SCORECARD IS UPLOADED ON-CHAIN HERE THAT SAYS HOW THE
+            <br />A FINAL SCORECARD IS UPLOADED ON-CHAIN THAT SAYS HOW THE
             GAME’S TREASURY SHOULD BE SHARED.
             <br />
             <br />
             NFT HOLDERS FROM ALL TEAMS ATTEST TO THE CORRECT SCORECARD TO RATIFY
-            IT, HERE. <br />
+            IT. <br />
             <br />
             BURN YOUR TEAM’S NFT TO RECLAIM ETH FROM THE GAME AT ANY TIME AFTER
-            A SCORECARD HAS BEEN RATIFIED, HERE.
+            A SCORECARD HAS BEEN RATIFIED
           </p>
         </div>
       </div>
       <br />
-      *THE OUTCOME IS SUBJECT TO THE RATIFIED SCORECARD DURING PHASE 4.{" "}
+      <span className={styles.disclaimer}>
+        {" "}
+        *THE OUTCOME IS SUBJECT TO THE RATIFIED SCORECARD DURING PHASE 4.
+      </span>
     </>
   );
 };
--- src/components/UI/Content/index.tsx
+++ src/components/UI/Content/index.tsx
@@ -9,30 +9,29 @@ import styles from "./Content.module.css";
 const Content: React.FC<PropsWithChildren<any>> = (props) => {
   return (
     <>
-    <hr className={styles.line} />
-    <div className={styles.accordian}>
-      <input
-        type="checkbox"
-        name="accordian"
-        id={props.title}
-        className={styles.accordianInput}
-      />
-      <label htmlFor={props.title} className={styles.accordianLabel}>
-        <div className={styles.contentHeader}>
-          {" "}
-          <h1 className={utilStyles.contentTitle}>{props.title} </h1>
-          <FontAwesomeIcon
-            icon={faChevronDown}
-            size="sm"
-            color={constants.contentTitleColor}
-            className={styles.chevronDown}
-          />
-        </div>
-      </label>
-      <div className={styles.content}>
-        {props.children} 
+      <hr className={styles.line} />
+      <div className={styles.accordian}>
+        <input
+          type="checkbox"
+          name="accordian"
+          id={props.title}
+          className={styles.accordianInput}
+          defaultChecked
+        />
+        <label htmlFor={props.title} className={styles.accordianLabel}>
+          <div className={styles.contentHeader}>
+            {" "}
+            <h1 className={utilStyles.contentTitle}>{props.title} </h1>
+            <FontAwesomeIcon
+              icon={faChevronDown}
+              size="sm"
+              color={constants.contentTitleColor}
+              className={styles.chevronDown}
+            />
+          </div>
+        </label>
+        <div className={styles.content}>{props.children}</div>
       </div>
-    </div>
     </>
   );
 };
