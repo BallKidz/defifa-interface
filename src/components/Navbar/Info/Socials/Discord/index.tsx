7f1bc3c4ce13340eac751af2df6cf9105273064e
blazh <medenicablazo@gmail.com>
2022-10-19 21:57:42 +0200

    fixed discord link and typo

--- src/components/Navbar/Info/Socials/Discord/index.tsx
+++ src/components/Navbar/Info/Socials/Discord/index.tsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 const Discord = () => {
   return (
     <li>
-      <Link href="https://discord.gg/cBfv2rBaaM" passHref>
+      <Link
+        href="https://discord.com/channels/775859454780244028/1022899568402251837"
+        passHref
+      >
         <a target="_blank">Join the discord</a>
       </Link>
     </li>
