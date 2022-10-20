e8d66ebce3d9015b2790afc7c88741a8106b6805
Devian <devianyeth@gmail.com>
2022-10-20 11:55:04 +0530

    Minor changes

--- src/components/SelfReferee/index.tsx
+++ src/components/SelfReferee/index.tsx
@@ -7,51 +7,48 @@ import styles from "./SelfReferee.module.css";
 
 const SelfRefree = () => {
   return (
-    <>
-      <Content title="Self-Refereeing">
-        <div className={styles.selfReferee}>
-          <p>
-            Defifa relies on the integrity of a few transactions made by the
-            game’s participants.
-          </p>
-          <p>
-            Scorecards can be submitted that suggest the correct results of
-            off-chain events.
-          </p>
-          <Button onClick={() => {}} size="big">
-            Submit a scorecard
-          </Button>
-          <br />
-          <br />
-          <p>
-            If you hold an nft, you can send a transaction attesting to a
-            submitted scorecard that conveys correct results of off-chain
-            events.
-          </p>
-          <p className={styles.attestationConfirm}>
-            You’ve attested to scorecard with root <b>0xasdf...1234</b> which
-            matches these results.
-          </p>
-          <Button onClick={() => {}} size="big">
-            CHANGE ATTESTATION
-          </Button>
-          <br />
-          <br />
-          <p>
-            Each game phase must also be queued by someone in the public in a
-            timely manner.
-          </p>
-          <Button onClick={() => {}} size="medium">
-            QUEUE PHASE 4
-          </Button>
-          <br />
-          <br />
-          <Button onClick={() => {}} size="big" color="#736B6F">
-            PHASE 4 ALREADY QUEUED
-          </Button>
-        </div>
-      </Content>
-    </>
+    <Content title="Self-Refereeing">
+      <div className={styles.selfReferee}>
+        <p>
+          Defifa relies on the integrity of a few transactions made by the
+          game’s participants.
+        </p>
+        <p>
+          Scorecards can be submitted that suggest the correct results of
+          off-chain events.
+        </p>
+        <Button onClick={() => {}} size="big">
+          Submit a scorecard
+        </Button>
+        <br />
+        <br />
+        <p>
+          If you hold an nft, you can send a transaction attesting to a
+          submitted scorecard that conveys correct results of off-chain events.
+        </p>
+        <p className={styles.attestationConfirm}>
+          You’ve attested to scorecard with root <b>0xasdf...1234</b> which
+          matches <span style={{fontWeight:"bold",textDecoration:"underline"}}>these results</span>.
+        </p>
+        <Button onClick={() => {}} size="big">
+          CHANGE ATTESTATION
+        </Button>
+        <br />
+        <br />
+        <p>
+          Each game phase must also be queued by someone in the public in a
+          timely manner.
+        </p>
+        <Button onClick={() => {}} size="medium">
+          QUEUE PHASE 4
+        </Button>
+        <br />
+        <br />
+        <Button onClick={() => {}} size="big" color="#736B6F">
+          PHASE 4 ALREADY QUEUED
+        </Button>
+      </div>
+    </Content>
   );
 };
 
