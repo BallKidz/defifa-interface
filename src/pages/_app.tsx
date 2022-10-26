ad0f0af972a9f7de5c98d4ca363d133c52febe37
blazh <medenicablazo@gmail.com>
2022-10-26 13:32:26 +0200

    adding fathomy analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,12 @@ import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import "../styles/globals.css";
+import * as Fathom from "fathom-client";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   const { chains, provider } = configureChains(
     [chain.mainnet],
     [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
@@ -23,6 +27,30 @@ function MyApp({ Component, pageProps }: AppProps) {
     connectors,
     provider,
   });
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    // Example: yourdomain.com
+    //  - Do not include https://
+    //  - This must be an exact match of your domain.
+    //  - If you're using www. for your domain, make sure you include that here.
+    Fathom.load("WIPQWWMN", {
+      includedDomains: ["www.defifa.net"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
