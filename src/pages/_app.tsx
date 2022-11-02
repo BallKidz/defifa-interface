9020f75df2fff8b1bdc9007574527405b781b134
blazh <medenicablazo@gmail.com>
2022-11-02 08:48:06 +0100

    adding keys

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,18 @@ import type { AppProps } from "next/app";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
+import { infuraProvider } from "wagmi/providers/infura";
 import { publicProvider } from "wagmi/providers/public";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { chains, provider } = configureChains(
-    [chain.mainnet],
-    [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+    [chain.mainnet, chain.goerli],
+    [
+      alchemyProvider({ apiKey: "MdEnS2hppeVnx8sqq4FvS6G3sH2HuUu4" }),
+      infuraProvider({ apiKey: "53e38b068eac401fabf933922b1673d6" }),
+      publicProvider(),
+    ]
   );
 
   const { connectors } = getDefaultWallets({
