68567292da9f4feb55feeeaf75df2e03207dbcca
Devian <devianyeth@gmail.com>
2022-11-05 22:07:29 +0530

    mint fix

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -1,1 +1,44 @@
-export const ETH_TOKEN_ADDRESS = '0x000000000000000000000000000000000000eeee'
+import { chain } from "wagmi";
+import { abi } from "@jbx-protocol/juice-721-delegate/out/IJBTiered721DelegateStore.sol/IJBTiered721DelegateStore.json";
+import {
+  DEFIFA_PROJECT_ID_GOERLI,
+  DEFIFA_PROJECT_ID_MAINNET,
+} from "./constants";
+import JBControllerMainnet from "@jbx-protocol/juice-contracts-v3/deployments/mainnet/JBController.json";
+import JBControllerGoerli from "@jbx-protocol/juice-contracts-v3/deployments/goerli/JBController.json";
+import MainnetJBETHPaymentTerminal from "@jbx-protocol/juice-contracts-v3/deployments/mainnet/JBSingleTokenPaymentTerminalStore.json";
+import GoerliJBETHPaymentTerminal from "@jbx-protocol/juice-contracts-v3/deployments/goerli/JBSingleTokenPaymentTerminalStore.json";
+export const ETH_TOKEN_ADDRESS = "0x000000000000000000000000000000000000eeee";
+export const goerliData = {
+  JBTiered721DelegateStore: {
+    address: "0x3ea16deff07f031e86bd13c55961eb576cd579a6",
+    interface: abi,
+  },
+  chainId: chain.goerli.id,
+  JBController: JBControllerGoerli,
+  projectId: DEFIFA_PROJECT_ID_GOERLI,
+  ethPaymentTerminal: GoerliJBETHPaymentTerminal,
+};
+
+export const mainnetData = {
+  JBTiered721DelegateStore: {
+    address: "0xffb2cd8519439a7ddcf2c933caedd938053067d2",
+    interface: abi,
+  },
+  chainId: chain.mainnet.id,
+  JBController: JBControllerMainnet,
+  projectId: DEFIFA_PROJECT_ID_MAINNET,
+  ethPaymentTerminal: MainnetJBETHPaymentTerminal,
+};
+
+export const mainnet = false;
+
+export function getChainData(chainId?: number) {
+  if (chainId === chain.mainnet.id) {
+    return mainnetData;
+  }
+  if (chainId === chain.goerli.id) {
+    return goerliData;
+  }
+  return mainnet ? mainnetData : goerliData;
+}
--- src/hooks/read/PaymentTerminalBalance.ts
+++ src/hooks/read/PaymentTerminalBalance.ts
@@ -1,28 +1,10 @@
 import { useContractRead, useNetwork } from "wagmi";
-import {
-  DEFIFA_PROJECT_ID_GOERLI,
-  DEFIFA_PROJECT_ID_MAINNET,
-} from "../../constants/constants";
-import MainnetJBETHPaymentTerminal from "@jbx-protocol/juice-contracts-v3/deployments/mainnet/JBSingleTokenPaymentTerminalStore.json";
-import GoerliJBETHPaymentTerminal from "@jbx-protocol/juice-contracts-v3/deployments/goerli/JBSingleTokenPaymentTerminalStore.json";
+import { getChainData } from "../../constants/addresses";
 
 export function usePaymentTerminalBalance() {
-  const { chain } = useNetwork();
-  const { ethPaymentTerminal, projectId } =
-    chain?.name === "mainnet"
-      ? {
-          ethPaymentTerminal: MainnetJBETHPaymentTerminal,
-          projectId: DEFIFA_PROJECT_ID_MAINNET,
-        }
-      : chain?.name === "goerli"
-      ? {
-          ethPaymentTerminal: GoerliJBETHPaymentTerminal,
-          projectId: DEFIFA_PROJECT_ID_GOERLI,
-        }
-      : {
-          ethPaymentTerminal: MainnetJBETHPaymentTerminal,
-          projectId: DEFIFA_PROJECT_ID_MAINNET,
-        };
+  const network = useNetwork();
+  const chainData = getChainData(network?.chain?.id);
+  const { ethPaymentTerminal, projectId } = chainData;
   return useContractRead({
     addressOrName: ethPaymentTerminal.address,
     contractInterface: ethPaymentTerminal.abi,
