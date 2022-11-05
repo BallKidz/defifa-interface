510027761cc62c1ba6d01dd0c86291bd05e8d096
blazh <medenicablazo@gmail.com>
2022-11-05 17:11:02 +0100

    adding current pot

--- src/hooks/read/PaymentTerminalBalance.ts
+++ src/hooks/read/PaymentTerminalBalance.ts
@@ -3,24 +3,33 @@ import {
   DEFIFA_PROJECT_ID_GOERLI,
   DEFIFA_PROJECT_ID_MAINNET,
 } from "../../constants/constants";
-import MainnetJBETHPaymentTerminal from "@jbx-protocol/juice-contracts-v3/deployments/mainnet/JBSingleTokenPaymentTerminalStore.json";
-import GoerliJBETHPaymentTerminal from "@jbx-protocol/juice-contracts-v3/deployments/goerli/JBSingleTokenPaymentTerminalStore.json";
+import MainnetJBSingleTokenPaymentTerminalStore from "@jbx-protocol/juice-contracts-v3/deployments/mainnet/JBSingleTokenPaymentTerminalStore.json";
+import GoerliJJBSingleTokenPaymentTerminalStore from "@jbx-protocol/juice-contracts-v3/deployments/goerli/JBSingleTokenPaymentTerminalStore.json";
+import GoerliJBETHPaymentTerminal from "@jbx-protocol/juice-contracts-v3/deployments/goerli/JBETHPaymentTerminal.json";
+import MainnetJBETHPaymentTerminal from "@jbx-protocol/juice-contracts-v3/deployments/mainnet/JBETHPaymentTerminal.json";
 
 export function usePaymentTerminalBalance() {
   const { chain } = useNetwork();
-  const { ethPaymentTerminal, projectId } =
+  const { JBSingleTokenPaymentTerminalStore, projectId } =
     chain?.name === "mainnet"
       ? {
-          ethPaymentTerminal: MainnetJBETHPaymentTerminal,
+          JBSingleTokenPaymentTerminalStore:
+            MainnetJBSingleTokenPaymentTerminalStore,
           projectId: DEFIFA_PROJECT_ID_MAINNET,
         }
       : {
-          ethPaymentTerminal: GoerliJBETHPaymentTerminal,
+          JBSingleTokenPaymentTerminalStore:
+            GoerliJJBSingleTokenPaymentTerminalStore,
           projectId: DEFIFA_PROJECT_ID_GOERLI,
         };
+  const ethPaymentTerminal =
+    chain?.name === "mainnet"
+      ? MainnetJBETHPaymentTerminal
+      : GoerliJBETHPaymentTerminal;
+
   return useContractRead({
-    addressOrName: ethPaymentTerminal.address,
-    contractInterface: ethPaymentTerminal.abi,
+    addressOrName: JBSingleTokenPaymentTerminalStore.address,
+    contractInterface: JBSingleTokenPaymentTerminalStore.abi,
     functionName: "balanceOf",
     args: projectId ? [ethPaymentTerminal.address, projectId] : null,
   });
