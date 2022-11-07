49e9fa1eb57d97e0064b8fa03420f87519e1ec5b
Devian <devianyeth@gmail.com>
2022-11-08 01:09:35 +0530

    usePay refactors

---
+++ src/utils/toast.ts
@@ -0,0 +1,14 @@
+import { toast } from "react-toastify";
+
+export function toastError(error: string) {
+  toast.error(error, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+}
