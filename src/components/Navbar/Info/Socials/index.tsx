b5fe3a9589a79c893198549b219a8b86c8d43a66
blazh <medenicablazo@gmail.com>
2022-11-09 12:15:20 +0100

    update socials

--- src/components/Navbar/Info/Socials/index.tsx
+++ src/components/Navbar/Info/Socials/index.tsx
@@ -13,7 +13,7 @@ const Socials = () => {
         </Link>
       </p>
       <p>
-        <Link href="https://www.juicebox.money/v2/p/303" passHref>
+        <Link href="https://www.juicebox.money/v2/p/305" passHref>
           <a target="_blank">Juicebox</a>
         </Link>
       </p>
