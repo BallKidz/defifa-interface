ac948be2637bfff0ffdb9011012e709ecebc3ced
blazh <medenicablazo@gmail.com>
2022-12-14 12:31:02 +0100

    remove logging

--- src/components/Mint/index.tsx
+++ src/components/Mint/index.tsx
@@ -40,8 +40,6 @@ const Mint = () => {
       (a: { minted: number }, b: { minted: number }) => b.minted - a.minted
     );
 
-  console.log({ rewardTiers });
-
   const { write, isLoading, isSuccess, isError } = usePay({
     amount: BigNumber.from(MINT_PRICE).mul(`${tierIds.length}`).toString(),
     token: ETH_TOKEN_ADDRESS,
