f6e9658b6dd8dc1b43fbdc826bb072087a157b1f
i001962@gmail.com <i001962@gmail.com>
2023-01-26 10:34:44 -0800

    put score component on main page. remove /simulate

--- src/pages/simulate/index.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from "next";
-import SimWrapper from "../../components/Simulator";
-
-const Simulate: NextPage = () => {
-  return <SimWrapper />;
-};
-
-export default Simulate;
