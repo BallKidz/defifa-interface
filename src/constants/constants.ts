67f97520d837c3e8dad18bd30a4a1ff1cec6840b
i001962@gmail.com <i001962@gmail.com>
2023-05-16 10:48:53 -0700

    bump and update category [0]

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
 
-export const DEFIFA_PROJECT_ID_GOERLI = 707;
+export const DEFIFA_PROJECT_ID_GOERLI = 708;
 export const DEFIFA_PROJECT_ID_MAINNET = 396;
 export const MINT_PRICE = ethers.utils.parseEther("0.01");
 
