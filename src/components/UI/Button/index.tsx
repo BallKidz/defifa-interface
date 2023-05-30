e98d9d1c82b487d88dd0c8e3021bc4a10c494dc2
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-05-30 21:09:40 +0930

    rounded buttons

--- src/components/UI/Button/index.tsx
+++ src/components/UI/Button/index.tsx
@@ -22,7 +22,7 @@ const Button = ({
   return (
     <button
       className={twJoin(
-        "rounded-sm  px-3 py-2 text-sm  shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600",
+        "rounded-full px-3 py-2 text-sm shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600",
         variant === "primary"
           ? props.disabled
             ? PRIMARY_BUTTON_DISABLED_CLASS
