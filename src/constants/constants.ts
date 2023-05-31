628a798aceeda706ca93058f40f75d01d20f77ba
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-05-31 16:24:02 +0930

    flip ff around

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,4 +2,4 @@ import { ethers } from "ethers";
 
 export const MINT_PRICE = ethers.utils.parseEther("0.01");
 export const NEW_GAME_DASHBOARD =
-  process.env.NEXT_PUBLIC_NEW_GAME_DASHBOARD === "true";
+  process.env.NEXT_PUBLIC_OLD_GAME_DASHBOARD !== "true";
