ada490191a8c60dcad452193ade7527cccbf4e7d
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-05-31 12:32:53 +0930

    make ff an env var

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,5 @@
 import { ethers } from "ethers";
 
 export const MINT_PRICE = ethers.utils.parseEther("0.01");
-export const NEW_GAME_DASHBOARD = false;
+export const NEW_GAME_DASHBOARD =
+  process.env.NEXT_PUBLIC_NEW_GAME_DASHBOARD === "true";
