f5c103d31752d3a00bb3032bfaa8cb5070e12728
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-06-02 08:21:53 +0930

    countdown loading state

--- src/components/NewGameDashboard/GameContainer/PlayContent/CountdownPhaseContent.tsx
+++ src/components/NewGameDashboard/GameContainer/PlayContent/CountdownPhaseContent.tsx
@@ -1,15 +1,17 @@
+import Container from "components/UI/Container";
 import { useCountdown } from "hooks/Countdown";
 import { useGameTimes } from "hooks/read/GameTimes";
 
 export function CountdownPhaseContent() {
-  const { data } = useGameTimes();
+  const { data, isLoading } = useGameTimes();
   const mintStart = new Date(
     (data?.start - data?.mintDuration - data?.refundDuration) * 1000
   );
 
   const { timeRemaining } = useCountdown(mintStart);
 
-  if (!data) return <>...</>;
+  if (!data || !isLoading)
+    return <Container className="text-center">...</Container>;
 
   return (
     <div className="flex justify-center">
