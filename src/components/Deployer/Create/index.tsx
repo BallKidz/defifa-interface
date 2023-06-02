6e660a130e0cb5c99f9703912d6c949635c30cef
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-06-02 11:44:55 +0930

    burp

--- src/components/Deployer/Create/index.tsx
+++ src/components/Deployer/Create/index.tsx
@@ -36,7 +36,7 @@ const datetimeLocalToUnix = (value: string): number => {
 };
 
 const DEFAULT_MINT_DURATION_SECONDS = 60 * 60; // 1 hour
-const DEFAULT_REFUND_DURATION_SECONDS = 60 * 60 * 0.5; // 1 hour
+const DEFAULT_REFUND_DURATION_SECONDS = 60 * 60; // 1 hour
 const GAME_START_BUFFER_SECONDS = 60 * 1; // 1 minute
 // const DEFAULT_MINT_DURATION_SECONDS = 60 * 60 * 72; // 1 hour
 // const DEFAULT_REFUND_DURATION_SECONDS = 60 * 60; // 1 hour
@@ -62,9 +62,8 @@ const DeployerCreate = () => {
   const [isUploading, setIsUploading] = useState(false);
   const [imageUri, setImageUri] = useState<any>();
   const [formValues, setFormValues] = useState<DefifaLaunchProjectData>({
-    name: "NBA Finals 2023: GAME 1",
-    rules:
-      "Heat vs Nuggets Game 1. Mint which team you think will win. If you're right, you'll get a share of total pot.",
+    name: "Your game name",
+    rules: "Your game rules",
     mintDuration: DEFAULT_MINT_DURATION_SECONDS,
     refundDuration: DEFAULT_REFUND_DURATION_SECONDS,
     start:
