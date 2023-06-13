710b3b37f6caf68cdc1a7f0fd3397a62331d03d7
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-06-13 10:33:06 +0930

    somehwat fix countdown

--- src/components/GameDashboard/GameContainer/PlayContent/MintPhase/MintCard.tsx
+++ src/components/GameDashboard/GameContainer/PlayContent/MintPhase/MintCard.tsx
@@ -5,7 +5,7 @@ export function MintCard({
   mintedCount,
   tierId,
   ...props
-}: { mintedCount: number, tierId: number } & Omit<PickCardProps, "extra">) {
+}: { mintedCount: number; tierId: number } & Omit<PickCardProps, "extra">) {
   const {
     nfts: { totalSupply, tiers: nfts },
   } = useGameContext();
@@ -14,24 +14,23 @@ export function MintCard({
     mintedCount > 0 && totalSupply
       ? ((mintedCount / totalSupply?.toNumber()) * 100).toFixed(0)
       : 0;
-  const supplyPortionString = nfts && nfts?.[tierId - 1]?.minted > 0 && totalSupply
-    ? `${mintedCount /nfts?.[tierId - 1]?.minted * 100}% of this card`
-    : `0%`;
+  const supplyPortionString =
+    nfts && nfts?.[tierId - 1]?.minted > 0 && totalSupply
+      ? `${(mintedCount / nfts?.[tierId - 1]?.minted) * 100}% of this card`
+      : `0%`;
 
-    return (
-      <PickCard
-        extra={
-          <>
-          <div>{mintedCount} minted by you </div>
+  return (
+    <PickCard
+      extra={
+        <>
+          <div>{mintedCount} mints</div>
           <div>{supplyPortion}% of game mints</div>
           {nfts && nfts?.[tierId - 1]?.minted > 0 && (
-            <div className="text-sm">
-              {supplyPortionString}
-            </div>
+            <div className="text-sm">{supplyPortionString}</div>
           )}
-        </>  
-        }
-        {...props}
-      />
-    );
+        </>
+      }
+      {...props}
+    />
+  );
 }
