ea4f67c939fa21aff2c2e70b312840bb278fee84
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-06-13 10:14:44 +0930

    quorum fix

--- src/components/GameDashboard/GameContainer/RulesContent/RulesContent.tsx
+++ src/components/GameDashboard/GameContainer/RulesContent/RulesContent.tsx
@@ -1,15 +1,14 @@
 import Container from "components/layout/Container";
 import { useGameContext } from "contexts/GameContext";
+import { useDefaultTokenBeneficiary } from "hooks/read/DefaultTokenBeneficiary";
 import { useDeployerDates } from "hooks/read/DeployerDates";
-import { useGameMetadata } from "hooks/read/useGameMetadata";
-import { useProjectCurrentFundingCycle } from "hooks/read/useProjectCurrentFundingCycle";
-import styles from "./index.module.css";
+import { useTiersOf } from "hooks/read/JB721Delegate/useTiersOf";
 import { useMaxTiers } from "hooks/read/MaxTiers";
 import { useTierBeneficiaries } from "hooks/read/TierBeneficiaries";
-import { useDefaultTokenBeneficiary } from "hooks/read/DefaultTokenBeneficiary";
-import { useTiersOf } from "hooks/read/JB721Delegate/useTiersOf";
-import Image from "next/image";
+import { useGameMetadata } from "hooks/read/useGameMetadata";
+import { useProjectCurrentFundingCycle } from "hooks/read/useProjectCurrentFundingCycle";
 import EnsName from "../ActivityContent/EnsName";
+import styles from "./index.module.css";
 
 export function RulesContent() {
   const { metadata, gameId, nfts } = useGameContext();
@@ -63,89 +62,117 @@ export function RulesContent() {
       <p className="mb-5">{metadata?.description}</p>
       <div className="p-4 border border-gray-800 rounded-lg mb-5 flex flex-col gap-2">
         <div>
-          <span className="text-pink-500">Phase 1:  </span>MINTS OPEN (mints open, refunds open)
+          <span className="text-pink-500">Phase 1: </span>MINTS OPEN (mints
+          open, refunds open)
           <span className={pillStyle(mintDuration.phase)}>
             {fillPill(mintDuration.phase)}
           </span>
         </div>
         <div>
-        <span className="text-pink-500">Phase 2:  </span>REFUNDS OPEN (mints closed)
+          <span className="text-pink-500">Phase 2: </span>REFUNDS OPEN (mints
+          closed)
           <span className={pillStyle(refundDuration.phase)}>
             {fillPill(refundDuration.phase)}
           </span>
         </div>
         <div>
           <div>
-          <span className="text-pink-500">Phase 3:  </span>SCORING (refunds closed)
+            <span className="text-pink-500">Phase 3: </span>SCORING (refunds
+            closed)
             <span className={pillStyle(start.phase)}>
               {fillPill(start.phase)}
             </span>
           </div>
           <div>
-            Anyone may submit a scorecard. Scorecards must be ratified by a majority of the Players.
+            Anyone may submit a scorecard. Scorecards must be ratified by a
+            majority of the Players.
           </div>
         </div>
       </div>
       <div>
         <div className="p-4 border border-gray-800 rounded-lg mb-5">
           <p>
-            <span className="text-pink-500">Winners: </span>Claim prize anytime after a scorecard has been ratified.
-            Redeeming a player card will burn it and transfer you its share of the pot.
+            <span className="text-pink-500">Winners: </span>Claim prize anytime
+            after a scorecard has been ratified. Redeeming a player card will
+            burn it and transfer you its share of the pot.
           </p>
           <p>
-          <span className="text-pink-500">No Contest: </span>
-            This occurs when nobody queues the next game phase.
-            Players may redeemed or keep their playing cards.
+            <span className="text-pink-500">No Contest: </span>
+            This occurs when nobody queues the next game phase. Players may
+            redeemed or keep their playing cards.
           </p>
         </div>
       </div>
       <div className="p-4 border border-gray-800 rounded-lg mb-5">
-  <div className="flex items-center">
-    <span className="text-pink-500">Mint Fees:</span>
-    {metadata?.seller_fee_basis_points === 0 ? (
-      <span className="ml-2">No fees are collected</span>
-    ) : (
-      <>
-        {/* <EnsName address={metadata?.fee_recipient} /> */}
-        <EnsName address={tokenBeneficiary?.toString()} />
-        <span className="ml-2">collects a {metadata?.seller_fee_basis_points}% fee on each pick minted.</span>
-      </>
-    )}
-  </div>
-  <div>
-  <div className="flex items-center">
-    <span className="text-pink-500">Reserved Mints:</span>
-    {nftRewardTiers?.filter((tier) => tier.reservedRate.toNumber() > 0).length === 0 ? (
-      <p className="ml-2">No reserve tokens are minted in this game</p>
-    ) : null}
-  </div>
-  {nftRewardTiers && nftRewardTiers?.filter((tier) => tier?.reservedRate.toNumber() > 0).length > 0 && (
-    <div className="flex flex-wrap">
-      {nftRewardTiers
-        ?.filter((tier) => tier.reservedRate.toNumber() > 0)
-        .map((tier, index) => {
-          const matchingTier = nfts?.tiers?.find((t) => t.id === tier.id.toNumber());
-          const imageSrc = matchingTier ? matchingTier.teamImage : "";
+        <div className="flex items-center">
+          <span className="text-pink-500">Mint Fees:</span>
+          {metadata?.seller_fee_basis_points === 0 ? (
+            <span className="ml-2">No fees are collected</span>
+          ) : (
+            <>
+              {/* <EnsName address={metadata?.fee_recipient} /> */}
+              <EnsName address={tokenBeneficiary?.toString()} />
+              <span className="ml-2">
+                collects a {metadata?.seller_fee_basis_points}% fee on each pick
+                minted.
+              </span>
+            </>
+          )}
+        </div>
+        <div>
+          <div className="flex items-center">
+            <span className="text-pink-500">Reserved Mints:</span>
+            {nftRewardTiers?.filter((tier) => tier.reservedRate.toNumber() > 0)
+              .length === 0 ? (
+              <p className="ml-2">No reserve tokens are minted in this game</p>
+            ) : null}
+          </div>
+          {nftRewardTiers &&
+            nftRewardTiers?.filter((tier) => tier?.reservedRate.toNumber() > 0)
+              .length > 0 && (
+              <div className="flex flex-wrap">
+                {nftRewardTiers
+                  ?.filter((tier) => tier.reservedRate.toNumber() > 0)
+                  .map((tier, index) => {
+                    const matchingTier = nfts?.tiers?.find(
+                      (t) => t.id === tier.id.toNumber()
+                    );
+                    const imageSrc = matchingTier ? matchingTier.teamImage : "";
 
-          return (
-            <div key={tier.id.toNumber()} className="p-4 border border-pink-800 rounded-lg mb-5">
-              {/* <Image src={imageSrc} width={100} height={100} alt="" /> */}
-              <p>Pick: {tier.id.toNumber()}</p>
-              <div className="flex items-center">
-                <span><EnsName address={tier.reservedTokenBeneficiary} /></span>
-                <span className="ml-2">will receive {1 / (tier.reservedRate.toNumber() + 1) * 100}% of this pick's tokens.</span>
+                    return (
+                      <div
+                        key={tier.id.toNumber()}
+                        className="p-4 border border-pink-800 rounded-lg mb-5"
+                      >
+                        {/* <Image src={imageSrc} width={100} height={100} alt="" /> */}
+                        <p>Pick: {tier.id.toNumber()}</p>
+                        <div className="flex items-center">
+                          <span>
+                            <EnsName address={tier.reservedTokenBeneficiary} />
+                          </span>
+                          <span className="ml-2">
+                            will receive{" "}
+                            {(1 / (tier.reservedRate.toNumber() + 1)) * 100}% of
+                            this pick's tokens.
+                          </span>
+                        </div>
+                      </div>
+                    );
+                  })}
               </div>
-            </div>
-          );
-        })}
-    </div>
-  )}
-</div>
+            )}
+        </div>
+      </div>
 
-
-</div>
-
+      <div>
+        <a
+          href={`https://etherscan.io/address/${currentFc?.metadata.dataSource}`}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          View NFT on Etherscan
+        </a>
+      </div>
     </Container>
   );
-  
 }
