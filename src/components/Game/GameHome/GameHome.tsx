f14869637fed9c716e613be1228c3c48ba74e5bd
i001962@gmail.com <i001962@gmail.com>
2023-07-11 18:26:20 -0700

    game home rev

--- src/components/Game/GameHome/GameHome.tsx
+++ src/components/Game/GameHome/GameHome.tsx
@@ -12,6 +12,7 @@ import { useRouter } from "next/router";
 import { Header } from "./Header";
 import { ActivityContent } from "../GameDashboard/GameContainer/ActivityContent/ActivityContent";
 import Chat from "components/Chat/Chat";
+import { GamePlayerPowerLevel } from "components/LeaderBoard/GamePivot/GamePlayerPowerLevel";
 
 function GameButton({ game }: { game: any }) {
   return (
@@ -29,7 +30,6 @@ export function GameHome() {
   const title = metadata?.name
     ? `${metadata.name} | Defifa`
     : "Money Games with Friends | Defifa";
-
   return (
     <>
       <Head>
@@ -41,12 +41,11 @@ export function GameHome() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <div className="bg-gradient-to-b from-slate-950 to-black">
-        <div className="border-b border-neutral-900 text-xs text-neutral-400">
+      <div className="bg-gradient-to-b from-slate-950 to-black min-h-screen">
+        <div className="border-b border-neutral-900 text-sm text-neutral-400">
           <Container>
             <div className="flex overflow-auto">
-              <div className="px-6 py-1 border-r border-neutral-800 shrink-0">
+              <div className="px-6 py-1 border-r border-neutral-800">
                 <Link href="/arcade">All games</Link>
               </div>
               {games?.map((g) => (
@@ -55,9 +54,8 @@ export function GameHome() {
             </div>
           </Container>
         </div>
-
         <div className="border-b border-neutral-900 py-2">
-          <Container className="flex justify-between">
+          <Container className="flex justify-between items-center">
             <Link href="/">
               <Image
                 src="/assets/defifa_spinner.gif"
@@ -69,34 +67,27 @@ export function GameHome() {
             <div className="flex gap-6 items-center">
               <Link href="/about">
                 <a className="flex items-center gap-2 text-neutral-300 text-sm">
-                  <QuestionMarkCircleIcon className="h-4 w-4 inline" /> How it
-                  works
+                  <QuestionMarkCircleIcon className="h-4 w-4 inline" /> How it works
                 </a>
               </Link>
               <Wallet />
             </div>
           </Container>
         </div>
-
         <Container className="my-8">
           <Header />
         </Container>
         <div className="bg-neutral-950 py-10">
           <Container>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              <div>
-                <h2 className="text-xl">Rules</h2>
-                {metadata?.description}
+              <div className="col-span-2 rounded-lg">
+                <GamePlayerPowerLevel />
               </div>
               <Chat />
-              <div className="bg-neutral-900 rounded-lg p-4">
-                <ActivityContent />
-              </div>
             </div>
           </Container>
-        </div>
-
-      </div>
+        </div >
+      </div >
     </>
-  );
+  )
 }
